ee4d5536100aabea799db8830cc4989df8ab05c5
Candido25 <115950054+Candido25@users.noreply.github.com>
2026-03-07 20:54:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // Pega aquí la URL de implementación web de tu Google Apps Script
-  apiUrl: "https://script.google.com/macros/s/AKfycbxxxxxxx/exec",
+  apiUrl: https://script.google.com/macros/s/AKfycbyl4NnQaqGGR5Y_YLWs-lIL4pm-aP2ss-rn7YvOVmIJFK3runfPQZ0PExcX8fVK42AO/exec,
   // Número de WhatsApp de respaldo, sin signos ni espacios
   whatsapp: "51968481482"
 };
